7cec9af4dc5c5322347ff75bdfc7224b6f4a584c
Kai-Malte Böhling <Kaiske307@users.noreply.github.com>
2018-01-13 16:12:16 +0100

    Revert "Persist Photo Class"
    This reverts commit 12d528466ab3e7810f9983c404168d64a69012da.

--- src/main/java/org/wahlzeit/agents/PersistPhotoAgent.java
+++ src/main/java/org/wahlzeit/agents/PersistPhotoAgent.java
@@ -1,7 +1,7 @@
 package org.wahlzeit.agents;
 
-import org.wahlzeit.model.ShirtPhoto;
-import org.wahlzeit.model.ShirtPhotoManager;
+import org.wahlzeit.model.Photo;
+import org.wahlzeit.model.PhotoManager;
 import org.wahlzeit.services.LogBuilder;
 
 import javax.servlet.ServletException;
@@ -28,12 +28,12 @@ public class PersistPhotoAgent extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-		String id = request.getParameter(ShirtPhoto.ID);
+		String id = request.getParameter(Photo.ID);
 		log.config(LogBuilder.createSystemMessage().addParameter("Try to persist PhotoId", id).toString());
 		if (id != null && !"".equals(id)) {
-			ShirtPhoto photo = (ShirtPhoto) ShirtPhotoManager.getInstance().getPhoto(id);
+			Photo photo = PhotoManager.getInstance().getPhoto(id);
 			if (photo != null) {
-				ShirtPhotoManager.getInstance().savePhoto(photo);
+				PhotoManager.getInstance().savePhoto(photo);
 				log.config(LogBuilder.createSystemMessage().addMessage("Photo saved.").toString());
 			} else {
 				response.setStatus(299);
